297908cc6504b06d18cee3e9006b7b033c99c5b9
Tawfique <tawfiquemt17@gmail.com>
2026-02-22 17:19:47 +0600

    Basic done

--- search_element_in_array.cpp
+++ search_element_in_array.cpp
@@ -1,11 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
-    int arr[100],n;
+    int arr[100],n,i;
     cin>>n;
-    for(int i=0; i<n; i++){
+    for(i=0; i<n; i++){
         cin>>arr[i];
+    }
+
+    int a;
+    cin>>a;
+
+    bool found = false;
+    for(i=0; i<n; i++){
+        if(a==arr[i]){
+            cout<< i << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if(!found){
+        cout<< -1<< endl;
     }
 
 }
