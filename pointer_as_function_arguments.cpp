cc67b3aceffd530869fc5daf201707c8fc820a97
Tawfique <tawfiquemt17@gmail.com>
2026-02-24 23:32:57 +0600

    added comment

--- pointer_as_function_arguments.cpp
+++ pointer_as_function_arguments.cpp
@@ -4,17 +4,19 @@ using namespace std;
 void increment(int *a){
     *a = *a + 1;
 }
-int second_increment(int x){
-    x = x + 1;
-    return x;
-}
+    int second_increment(int x){
+        x = x + 1;
+        return x;
+    }
 
-int main(){
-    int a = 23;
-    int x = 125;
-    increment(&a);
-    cout<< a <<endl;
+    int main(){
+        int a = 23;
+        int x = 125;
+        increment(&a);
+        cout<< a <<endl;
 
-    second_increment(x);
-    cout << x << endl;
-}
+        /* here when second_increment function is called we save it into a new variable y. 
+        That's how value is changed but x remains the same as we aren't passing the variable by reference */
+        int y = second_increment(x);
+        cout << y << endl;
+    }
