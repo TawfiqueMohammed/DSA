8a69f386466aa41c5f4880cc99f9b075c092225e
Tawfique <tawfiquemt17@gmail.com>
2026-02-25 06:30:10 +0600

    reverse order using pointer

--- pointers_and_array.cpp
+++ pointers_and_array.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main(){
     int A[5]={1,2,3,4,5};
+    int *p = &A[0]; // it's not needed as Array decay works here.
     /* int *p = &a[3];
     cout<<p<<" "<<a<<" "<<*p<<" "<<&a[0]<<endl; */
 
     for(int i=0; i<5; i++){
         cout<<"Address: "<< &A[i]<<" "<< "Value: "<< A[i] << endl;
         cout<<"Address: "<< A+i<<" "<< "Value: "<< *(A+i) << endl;
+    }
+
+    // showing the array values in reverse order by using pointer
+
+    int *q = A + 4;
+    cout<<"values: " << " ";
+    for (int i=0; i<5; i++){
+        cout<< *q << " ";
+        q--;
     }
 
 }
