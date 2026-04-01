cd48730217af4ce7d1147d5725c35e055816a673
Tawfique <tawfiquemt17@gmail.com>
2026-04-01 21:24:31 +0600

    maximum subarray using running sum

---
+++ maximum_subarray_using_running_sum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A[] = {1, -2, 3, 4, 5};
+    int n = sizeof(A) / sizeof(A[0]);
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        int currentSum = 0;
+        for (int end = start; end < n; end++)
+        {
+            currentSum += A[end];
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+    }
+
+    cout << maxSum << endl;
+}
