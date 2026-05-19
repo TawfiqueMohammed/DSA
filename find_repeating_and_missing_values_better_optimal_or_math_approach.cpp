53a53c2f6a670eac698f834f2dfb3bba86363ef9
Tawfique <tawfiquemt17@gmail.com>
2026-05-19 23:36:17 +0600

    done it by own

--- find_repeating_and_missing_values_better_optimal_or_math_approach.cpp
+++ find_repeating_and_missing_values_better_optimal_or_math_approach.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main(){
-    vector<int> v = {1, 2, 3, 4, 4, 5, 6, 8, 9};
+    vector<int> v = {1,2,3,4,4,5,6,7,9};
     int n = v.size();
+    int actualSum = 0, actualSqSum = 0;
 
-    long long actualSum = 0, actualSumSq = 0;
-
-    for(int i = 0; i < n; i++){
-        actualSum   += v[i];
-        actualSumSq += (long long)v[i] * v[i];
+    for(int i=0; i<n; i++){
+        actualSum += v[i];
+        actualSqSum += (long long)v[i]*v[i];
     }
 
-    long long expectedSum   = (long long)n * (n+1) / 2;
-    long long expectedSumSq = (long long)n * (n+1) * (2*n+1) / 6;
+    long long expectedSum = ((long long)n * (n+1)) / 2;
+    long long expectedSqSum = ((long long)n * (n+1) * (2*n+1)) / 6;
 
-    long long X = actualSum   - expectedSum;    // R - M
-    long long Y = (actualSumSq - expectedSumSq) / X;  // R + M
+    long long X = actualSum - expectedSum;
+    long long Y = (actualSqSum - expectedSqSum) / X;
 
-    int R = (X + Y) / 2;  // repeating
-    int M = Y - R;         // missing
+    int R = (X+Y)/2;
+    int M = Y-R;
 
-    cout << "Repeating: " << R << endl;
-    cout << "Missing:   " << M << endl;
+    cout << R << " " << M;
+ 
+    // cout << actualSum << " " << actualSqSum;
 }
