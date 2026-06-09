bf18cb11851e6a8d1feb9028876d9f61ca0c6f5c
Tawfique <tawfiquemt17@gmail.com>
2026-06-09 22:21:48 +0600

    3 sum better approach with hashing

---
+++ three_sum_better_with_hashing.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v = {-1,0,1,2,-1,-4};
+    int n = v.size();
+    set<vector<int>> uniqueTriplets;
+    for(int i = 0; i<n; i++){
+        int target = -v[i];
+        set<int> s;
+        for(int j = i+1; j<n; j++){
+            int c = target - v[j];
+            if(s.find(c) != s.end()){
+                vector<int> trip = {v[i], v[j], c};
+                sort(trip.begin(), trip.end());
+                uniqueTriplets.insert(trip);
+            }
+            s.insert(v[j]);
+        }
+    }
+
+    vector<vector<int>> ans(uniqueTriplets.begin(), uniqueTriplets.end());
+
+    for(int i = 0; i < ans.size(); i++) {
+        for(int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+}
